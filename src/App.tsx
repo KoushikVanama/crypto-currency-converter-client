15dc60d5fd672685b01b8afedb25002c0d9d3444
Sai Koushik <koushik.vanama@gmail.com>
2023-12-31 17:20:13 +0530

    chore: initial commit, basic form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useFormik } from 'formik';
 
-function App() {
+const App = () => {
+  const formik = useFormik({
+    initialValues: { currency: "", amount: "", crypto: "" },
+    onSubmit: values => {
+      console.log("Form submitted", values);
+    }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App text-white">
+      <form onSubmit={formik.handleSubmit}>
+        <div>
+          <label htmlFor='crypto'>
+            Crypto:
+          </label>
+          <input name="crypto" onChange={formik.handleChange} value={formik.values.crypto} />
+        </div>
+        <div>
+          <label htmlFor='currency'>
+            Currency:
+          </label>
+          <input name="currency" onChange={formik.handleChange} value={formik.values.currency} />
+        </div>
+        <div>
+          <label htmlFor='amount'>
+            Amount:
+          </label>
+          <input type="number" name="amount" onChange={formik.handleChange} value={formik.values.amount} />
+        </div>
+        <div>
+          <button type='submit'>Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
